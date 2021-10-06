1f6b7658f31b6db2fee29baceb09d3453f3d5e37
Mabel <mabelksouza@gmail.com>
2021-10-06 11:44:15 -0300

    alterei para ver o teste

--- PrimeiroApp-Curso/ViewController.swift
+++ PrimeiroApp-Curso/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResultado: UILabel!
+    @IBOutlet weak var idadeCachorro: UITextField!
+    @IBOutlet weak var nomeCachorro: UITextField!
+    @IBOutlet weak var labelNome: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(idadeCachorro.text!)! * 7;
+        let nome = nomeCachorro.text!;
+        
+        labelResultado.text = nome + " possui " + String(idade) + " anos em idade de humano..";
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
